df0cd10687bd2c64b3cb97df9dfb347685e2e289
natyboo5 <nparedes517@yahoo.com>
2024-01-12 16:10:05 -0500

    initial check in for project 1

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+// const professionalController = require('../controllers/professional');
+
+
+// GET /feed/posts
+router.get('/', (req, res) => { res.send('Hello World'); });
+
+module.exports = router;
