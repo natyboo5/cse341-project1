5a3d14e1b06b84d4439ebd90154ce9aeb390e8bf
natyboo5 <nparedes517@yahoo.com>
2024-01-17 11:11:33 -0500

    installed swagger-autogen and swagger-ui-express packages

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
-
-const mongodb = require('./data/database.js');
 const app = express();
+const bodyParser = require('body-parser');
+const mongodb = require('./data/database.js');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
 app.use('/', require('./routes'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 mongodb.initDb((err) => {
     if (err) {
@@ -17,3 +21,4 @@ mongodb.initDb((err) => {
    }
     
 });
+
