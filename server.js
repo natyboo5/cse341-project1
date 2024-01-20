794177fbf829d7adc0a83a67c54a90a15431f767
natyboo5 <nparedes517@yahoo.com>
2024-01-20 01:32:38 -0500

    CORS updated

--- server.js
+++ server.js
@@ -6,8 +6,16 @@ const mongodb = require('./data/database.js');
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(  //test
+    'Access-Control-Allow-Heaters',
+    'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
 app.use('/', require('./routes'));
-
 
 mongodb.initDb((err) => {
     if (err) {
